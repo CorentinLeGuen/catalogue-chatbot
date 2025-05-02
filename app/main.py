6bb218589d9b214544bc5b8fd3564d3e293f9d03
CorentinLeGuen <leguen.corentin@protonmail.com>
2025-05-02 16:43:35 -0400

    Refactoring main.py - update seach parameters

--- app/main.py
+++ app/main.py
@@ -21,9 +21,11 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 CATALOGUE_API = os.getenv("CATALOGUE_API")
 
 # === Shared memory (global) ===
-index = None
+INDEX_LOCK = threading.Lock()
+INDEX = None
 books = []
 books_ids = {}
+
 
 def embed_text(text: str) -> list:
     """
@@ -44,10 +46,10 @@ def build_faiss_index_from_api(api_url: str):
     :param api_url: The catalogue API URL.
     :return: Books index.
     """
-    global index, books, books_ids
+    global INDEX, books, books_ids
 
     try:
-        response = requests.get(api_url)
+        response = requests.get(api_url, timeout=10)
         response.raise_for_status()
         books_raw = response.json()
 
@@ -67,9 +69,10 @@ def build_faiss_index_from_api(api_url: str):
         idx = faiss.IndexIDMap(faiss.IndexFlatL2(dim))
         idx.add_with_ids(embeddings_np, np.array(ids, dtype=np.int64))
 
-        index = idx
-        books = books_raw
-        books_ids = {int(l["isbn"]): l for l in books_raw if "isbn" in l}
+        with INDEX_LOCK:
+            INDEX = idx
+            books = books_raw
+            books_ids = {int(l["isbn"]): l for l in books_raw if "isbn" in l}
 
     except Exception as e:
         print(f"Unable to update indexes : {e}")
@@ -82,6 +85,7 @@ def schedule_refresh(api_url: str, interval_seconds: int = 600):
     :param interval_seconds:
     :return:
     """
+
     def loop():
         while True:
             build_faiss_index_from_api(api_url)
@@ -90,30 +94,40 @@ def schedule_refresh(api_url: str, interval_seconds: int = 600):
     thread = threading.Thread(target=loop, daemon=True)
     thread.start()
 
+
 def chatbot_conversation(messages: List[dict]) -> str:
     """
     Generate a response by a message history.
-    :param messages: The messages history.
+    :param messages: The message history.
     :return: A response.
     """
-    if index is None:
-        raise RuntimeError("L'application est en cours d'initialisation. Revenez plus tard...")
+    if INDEX is None:
+        raise RuntimeError("L'application est en cours d'initialisation...")
+
     last_question = messages[-1]["content"]
 
-    vector = np.array([embed_text(last_question)], dtype=np.float32)
-    distance, ids = index.search(vector, k=5)
-    candidates = [books_ids[i] for i in ids[0] if i in books_ids]
-    book_text = "\n".join([
-        f"- {book['title']}: {book['summary']}" for book in candidates
-    ])
+    vector = np.array([embed_text(last_question)], dtype=np.float32).reshape(1, -1)
 
-    full_message =[{
+    with INDEX_LOCK:
+        _, ids = INDEX.search(x=vector, k=5)
+        candidates = [books_ids[i] for i in ids[0] if i in books_ids]
+
+    book_text = "\n".join(
+        [
+            f"- {book['title']}: {book['summary']}" for book in candidates
+        ]
+    )
+
+    full_message = [{
         "role": "system",
         "content": (
-            "Tu es un bibliothécaire nommé ChatXUM. "+
-            "Tu peux discuter librement, répondre à des questions sur les livres, recommander des livres si nécessaires. "+
-            "Voici ta base de livres:\n" +
-            (book_text or "pas encore de livres disponibles.")
+                "Tu es un bibliothécaire nommé ChatXUM. " +
+                "Tu peux discuter librement, répondre à des questions sur les livres, recommander des livres si nécessaires. " +
+                "Si tu ne trouves pas un livre, ou si on te demande, tu rafraichis ta base de livres toutes les 10 minutes." +
+                "Si on te demande ta technologie ou toute question relative à toi, tu es un bibliothécaire construit par Corentin Le Guen." +
+                f"Ta base de livres, contient {len(books_ids)} mais elle peut évoluer." +
+                "Voici ta base de livres:\n" +
+                (book_text or "pas encore de livres disponibles.")
         )
     }] + messages
 
@@ -136,13 +150,25 @@ async def lifespan(app: FastAPI):
     schedule_refresh(api_url)
     yield
 
+
 app = FastAPI(lifespan=lifespan)
+
 
 class ChatHistory(BaseModel):
     """
     Chat history with the user.
     """
     messages: List[dict]
+
+
+@app.get("/health")
+def get_health():
+    """
+    Simple endpoint to see if the server is up.
+    :return: up
+    """
+    return {"status": "up"}
+
 
 @app.post("/ask")
 def ask(history: ChatHistory):
@@ -151,7 +177,7 @@ def ask(history: ChatHistory):
     :param history: The chat history
     :return: A response.
     """
-    if index is None:
+    if INDEX is None:
         return {"message": "index not ready"}
 
     try:
